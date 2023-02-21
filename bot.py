af92ec62e6ed0f7f742502961d60c3d3ffee4831
Isamutdin <mr.aisamutdin@mail.ru>
2023-02-22 01:06:40 +0300

    Sucsess Video

--- bot.py
+++ bot.py
@@ -3,79 +3,82 @@ from aiogram import Bot, types
 from state import Video, PlayList
 from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.utils import executor
-from pars import download_video, download_playlist
+from pars import download_videoYT, download_playlistYT, check_videoYT, create_videoYT
 from os import remove
+
+#keyboard = types.ReplyKeyboardRemove(True)
+
 
 with open("TOKEN.txt", 'r') as f: 
     TOKEN = f.read()
-    
+
+
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-
-
-
-
-
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await message.answer("У бота 2 комманды \n/video-Скачать видео\n/playlist-Скачать плэйлист")
-    
-
-
-
-
-
-
+    await message.answer("У бота 2 комманды \n/video-Скачать видео\n/playlist-Скачать плэй лист")
 
 ###############################################################################
 @dp.message_handler(commands="video")
 async def state_v(message: types.Message):
-    await message.answer("Просто отправте ссылки на видео с ютуба, а в конце написать комманду /end")
-    await bot.delete_message(message.chat.id, message.message_id)
+    r = await message.answer("Просто отправь ссылки на видео с ютуба, а в конце написать комманду /end")
+    print(message)
+    await message.delete()
     await Video.video.set()
 
 @dp.message_handler(state=Video.video, commands='end')
 async def end_v(message: types.Message, state: FSMContext):
-    await bot.delete_message(message.chat.id, message.message_id)
+    await message.delete()
     await state.finish()
-
 
 @dp.message_handler(state=Video.video)
 async def douwnload_v(message: types.Message):
-    name = download_video(message.text)
-    await bot.send_audio(message.chat.id, open(name, 'rb'))
-    await bot.delete_message(message.chat.id, message.message_id)
-    remove(name)
-###############################################################################
+    url = message.text.split("&")[0].replace("www.", '')
+    r = check_videoYT(url.split("/")[-1])
+    if r:
+        await bot.send_audio(message.chat.id, r)
 
-
-###############################################################################
-@dp.message_handler(commands="playlist")
-async def state_pl(message: types.Message):
-    await message.answer("Просто отправте ссылки на видео с ютуба, а в конце написать комманду /end")
-    await bot.delete_message(message.chat.id, message.message_id)
-    await PlayList.playlist.set()
-
-@dp.message_handler(state=PlayList.playlist, commands='end')
-async def end_pl(message: types.Message, state: FSMContext):
-    await bot.delete_message(message.chat.id, message.message_id)
-    await state.finish()
-
-
-@dp.message_handler(state=PlayList.playlist)
-async def douwnload_pl(message: types.Message):
-    names = download_playlist(message.text)
-    for name in names:
-        await bot.send_audio(message.chat.id, open(name, 'rb'))
+    else:
+        name, info = download_videoYT(message.text)
+        result = await bot.send_audio(message.chat.id, open(name, 'rb'))
+        info['video_id'] = result.audio.file_id
+        info['telegram_id'] = message.from_id
         remove(name)
-    
-    await bot.delete_message(message.chat.id, message.message_id)
+        create_videoYT(info)
+    await message.delete()
 ###############################################################################
+
+
+###############################################################################
+# @dp.message_handler(commands="playlist")
+# async def state_pl(message: types.Message):
+#     await message.answer("Просто отправь ссылки на видео с ютуба, а в конце написать комманду /end")
+#     await message.delete()
+#     await PlayList.playlist.set()
+
+# @dp.message_handler(state=PlayList.playlist, commands='end')
+# async def end_pl(message: types.Message, state: FSMContext):
+#     await message.delete()
+#     await state.finish()
+
+# @dp.message_handler(state=PlayList.playlist)
+# async def douwnload_pl(message: types.Message):
+#     names = download_playlistYT(message.text)
+#     for name in names:
+#         cap = name.split("\\")[-1][:-4]
+#         await bot.send_audio(message.chat.id, open(name, 'rb'), caption=cap, title=cap)
+#         remove(name)
+
+#     await message.delete()
+###############################################################################
+    
 
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+    
--- pars.py
+++ pars.py
@@ -1,24 +1,78 @@
+from operator import mul
 from pytube import YouTube
 from pytube import Playlist
+import requests
 
+DOMEN = 'http://127.0.0.1:8000/'
+API = 'api/v1/videoYT/'
+APIUSER = 'api/v1/uservideoYT/'
 
-def download_video(url):
+def check_videoYT(yt_id):
+    data = {'yt_id': yt_id}
+    r = requests.get(DOMEN+API, data=data).json()['video_id']
+    if r != 'None': return r
+    return False
+
+def download_videoYT(url):
     yt = YouTube(url)
-    return yt.streams.filter(mime_type="audio/mp4").first().download()
+    info = {
+        'token': "",
+        'url': yt.watch_url,
+        'title': yt.title,
+        'url_img': f'http://img.youtube.com/vi/{yt.video_id}/mqdefault.jpg',
+        'author': yt.author,
+        'yt_id': yt.video_id
+    }
+
+    return yt.streams.filter(mime_type="audio/mp4").first().download(r'F:\music_bot\audio'), info
+
+def create_videoYT(info):
+    r = requests.post(DOMEN+API, data=info).json()
+    info['video'] = r['video']
+    requests.post(DOMEN+APIUSER, data=info)
+    return 'ok'
 
 
-def download_playlist(url):
+def download_playlistYT(url):
     pl = Playlist(url)
     spisok = []
     for video in pl.videos:
-        spisok.append(video.streams.filter(mime_type="audio/mp4").first().download())
+        spisok.append(video.streams.filter(mime_type="audio/mp4").first().download(r'F:\music_bot\audio'))
     return spisok
 
 
 
 
 
-
 if __name__ == "__main__":
-    url = 'https://www.youtube.com/playlist?list=PLqGS6O1-DZLo8fjcztVUM0Eotb1fT2ZOh'
-    print(download_playlist(url))
+    #5208514073:AAE4tGvLhsCeWAaR1Cuv8C30YSScqvuyuXk
+    #1770295162
+    #1705450152
+    #1063089542 - Саня
+    #585067098 - Катя
+    #692175727 - Матвей
+    # url = 'https://www.youtube.com/watch?v=oGT3Z7fVNc0'
+    # spsiok_urlYT = ['https://www.youtube.com/watch?v=ybgWXT768pA&list=RDMMjqdGiHPMy_c&index=7', 'https://www.youtube.com/watch?v=8uDftl_TJzY&list=RDMMjqdGiHPMy_c&index=10']
+    # for elem in spsiok_urlYT:
+    #     data = {
+    #         'token': '34wetaq35fc1243q',
+    #         'url': elem, #'https://www.youtube.com/watch?v=1YBpZBE3c2U&list=RDMMjqdGiHPMy_c&index=9', #'https://www.youtube.com/watch?v=XWJ1sn3QC2c',
+    #         'video_id':'CQACAgIAAxkDAAIKPWPvr1LxFSo-jVTqDw3h1TWyhviHAAI_IgACEYaBS5iVU8c4PZoRLgQ',
+    #         'title':'wercvzxvgz',
+    #         'url_img':'http://img.youtube.com/vi/fJc3WBiWEos/mqdefault.jpg',
+    #         'author': 'Swap',
+    #     }
+    data = {'token': '', 'url': 'https://youtube.com/watch?v=fC1HF29n9UA', 'title': 'DVRST, OBLXKQ - ENDLESS LOVE', 'url_img': 'http://img.youtube.com/vi/fC1HF29n9UA/mqdefault.jpg', 'author': 'DVRST', 'yt_id': 'fC1HF29n9UA', 'video_id': 'CQACAgIAAxkDAAILvGP1JxrLiGMR-1ugWuf5t71-NDsyAALyJgACPyqpS1U06JX96hR1LgQ', 'telegram_id': 1770295162}
+    r = (requests.post('http://127.0.0.1:8000/api/v1/videoYT/', data=data).json())
+    print(r)
+    data['video']= r['video']
+    print(requests.post(DOMEN+APIUSER, data=data).text)
+    # data = {
+    #     'audio': 'CQACAgIAAxkDAAIKLGPvq6fx9KFF2Tkla6GHnD3081dlAAL0IQACEYaBSw_w0_wwEGihLgQ',
+    #     'chat_id': '1705450152',
+    # }
+    # print(requests.post('https://api.telegram.org/bot1705450152:AAGnaIFa2xgob03m9dkD2Ja6QdDRL-fUyVA/sendAudio', data=data).text)
+    # yt = YouTube('https://www.youtube.com/watch?v=fJc3WBiWEos')
+    # v = yt.streams.filter(mime_type="audio/mp4").first().download(r'F:\music_bot\audio')
+    # print(yt.author, yt.watch_url)
+    
